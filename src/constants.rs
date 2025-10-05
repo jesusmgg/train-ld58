94288cee41191625dbafbb30ccd1bac1d8e168db
Jesus Gonzalez <jesus@jesusmg.net>
2025-10-04 23:14:36 -0300

    Change camera speed

--- src/constants.rs
+++ src/constants.rs
@@ -6,7 +6,7 @@ pub const TILE_SIZE_Y: f32 = TILE_SIZE_PX;
 pub const SCREEN_W: f32 = 512.0;
 pub const SCREEN_H: f32 = 288.0;
 
-pub const CAMERA_TRANSITION_SPEED: f32 = 0.15;
+pub const CAMERA_TRANSITION_SPEED: f32 = 0.1;
 
 pub const TRAIN_SPEED: f32 = 2.0; // Tiles per second
 pub const TRAIN_ANIM_SPEED: f32 = 0.15; // Seconds per frame
